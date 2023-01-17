440085a12b72ee097a1b79aabe013b030ae3bfeb
Yuzo1 <97038731+Yuzo1@users.noreply.github.com>
2023-01-17 13:23:06 +0300

    Update UserService.java

--- src/main/java/web/service/UserService.java
+++ src/main/java/web/service/UserService.java
@@ -5,8 +5,8 @@ import web.model.User;
 import java.util.List;
 
 public interface UserService {
-    List<User> listUsers();
-    User getUser(int id);
+   public List<User> listUsers();
+   public User getUser(int id);
     void update(int id, User user);
     void delete(int id);
     void add(User user);
