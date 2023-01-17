05b7601f9044fa1c0d806ebcc79d98d0f7a50a0f
Yuzo1 <97038731+Yuzo1@users.noreply.github.com>
2023-01-17 13:25:51 +0300

    Update UserServiceImp.java

--- src/main/java/web/service/UserServiceImp.java
+++ src/main/java/web/service/UserServiceImp.java
@@ -14,32 +14,26 @@ public class UserServiceImp implements UserService{
     @Autowired
     UserDAO userDAO;
 
-    
-    @Transactional
     @Override
     public List<User> listUsers() {
         return userDAO.listUsers();
     }
 
-    @Transactional
     @Override
     public User getUser(int id) {
         return userDAO.getUser(id);
     }
 
-    @Transactional
     @Override
     public void update(int id, User user) {
         userDAO.update(id, user);
     }
 
-    @Transactional
     @Override
     public void delete(int id) {
         userDAO.delete(id);
     }
 
-    @Transactional
     @Override
     public void add(User user) {
         userDAO.add(user);
